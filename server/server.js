e29038c3e415702bc8a27a4b38137dbd657307b0
Amir-Aminian <amir_aminian@hotmail.com>
2023-03-02 16:24:24 -0330

    created put method

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 global.config = require("./config");
+const {body, validationResult} = require("express-validator");
 let users = require("./users");
 
 const app = express();
@@ -19,10 +20,31 @@ app.get("/:id", (req, res) => {
   res.status(200).json(user);
 });
 
-app.post("/", (req, res) => {
-  req.body.id = parseInt(req.body.id);
-  users.push(req.body);
-  res.status(200).json("New user added successfully!");
+app.post(
+  "/", 
+  body("email", "Please use a correct email format!").isEmail(), 
+  body("password", "Password should be at least 8 characters long!").isLength({min: 8}), 
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return(res.status(400).json({errors: errors.array()}));
+    };
+    req.body.id = parseInt(req.body.id);
+    users.push(req.body);
+    res.status(200).json("Successfully added new user!");
+  }
+);
+
+app.put("/:id", (req, res) => {
+  users = users.map((user) => {
+    if (user.id == req.params.id) {
+      req.body.id = parseInt(req.body.id);
+      return(req.body);
+    } else {
+      return(user);
+    };
+  });
+  res.status(200).json(`Successfully updated user id: ${req.params.id}`);
 });
 
 app.listen(config.port, () => {
--- server/users.js
+++ server/users.js
@@ -3,24 +3,24 @@ module.exports = [
     id: 1,
     username: "Jack",
     email: "jack@gmail.com",
-    password: "1234"
+    password: "12345678"
   },
   {
     id: 2,
     username: "Brad",
     email: "brad@gmail.com",
-    password: "1234"
+    password: "12345678"
   },
   {
     id: 3,
     username: "Sarah",
     email: "sarah@gmail.com",
-    password: "1234"
+    password: "12345678"
   },
   {
     id: 4,
     username: "John",
     email: "john@gmail.com",
-    password: "1234"
+    password: "12345678"
   }
 ];
