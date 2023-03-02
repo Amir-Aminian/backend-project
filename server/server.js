a34881a5b0dc10e9ff2bd981d5859ccc1bb12783
Amir-Aminian <amir_aminian@hotmail.com>
2023-03-02 20:05:22 -0330

    created static public folder in client side

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const { stat } = require("fs");
+const path = require("path");
 global.config = require("./config");
 
 const app = express();
+const static__dir = path.resolve(path.join(__dirname, "../client/public"));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use("/", require("./routes/user"));
+app.use(express.static(static__dir));
+app.use("/user", require("./routes/user"));
 
 app.listen(config.port, () => {
   console.log(`server is runing on port: ${config.port}`);
+  console.log(static__dir);
 });
