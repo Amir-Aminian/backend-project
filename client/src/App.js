396df0ccf67e9e50556be666b0e956e8bfc2498a
Amir-Aminian <amir_aminian@hotmail.com>
2023-03-06 20:48:58 -0330

    created CRUD functions in client side and they are working properly

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,69 @@ function App() {
   const [users, setUsers] = useState(null);
 
   const url = "http://localhost:8080/user";
-  const request = new Request(url, {
-    method: "get"
+
+  const request1 = new Request(url, {
+    method: "GET"
+  });
+
+  const request2 = new Request(url, {
+    method: "POST",
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+      id: 5,
+    username: "amir",
+    email: "amir@gmail.com",
+    password: "12345678"
+    })
+  });
+
+  const request3 = new Request("http://localhost:8080/user/5", {
+    method: "DELETE"
+  });
+
+  const request4 = new Request("http://localhost:8080/user/5", {
+    method: "PUT",
+    headers: {'Content-Type':'application/json'},
+    body: JSON.stringify({
+      id: 5,
+    username: "brandon",
+    email: "brandon@gmail.com",
+    password: "12345678"
+    })
   });
   
-  async function getData() {
+  const getData = async (request) => {
     const response = await fetch(request);
     const data = await response.json();
     setUsers(data);
     console.log(data);
   };
 
+  const postData = async (request) => {
+    const response = await fetch(request);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  const deleteData = async (request) => {
+    const response = await fetch(request);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  const updateData = async (request) => {
+    const response = await fetch(request);
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <>
       <ShowUsers users={users} />
-      <button onClick={() => getData()}>Get All Users</button>
+      <button onClick={() => getData(request1)}>Get all users</button>
+      <button onClick={() => postData(request2)}>Add another user</button>
+      <button onClick={() => deleteData(request3)}>Delete a user</button>
+      <button onClick={() => updateData(request4)}>Update a user</button>
     </>
   );
 }
--- client/src/components/showUsers.js
+++ client/src/components/showUsers.js
@@ -5,7 +5,7 @@ const ShowUsers = ({users}) => {
         <h1>All Users:</h1>
         {users.map((user) => (
           Object.entries(user).map((user) => (
-            <h2>{user[0]}:  {user[1]}</h2>
+            <h5>{user[0]}:  {user[1]}</h5>
           ))
         ))}
       </>
