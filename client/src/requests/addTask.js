18be05fb15a5c742b1a319aeaf86c53409ed3f3c
Amir-Aminian <amir_aminian@hotmail.com>
2023-04-12 21:36:57 -0230

    created add task request in client side.

---
+++ client/src/requests/addTask.js
@@ -0,0 +1,16 @@
+const addTask = async (data) => {
+  const response = await fetch(
+    new Request(
+      "http://localhost:8080/api/addTask", 
+      {
+        method: "POST",
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(data)
+      }
+    )
+  );
+  const result = await response.json();
+  return(result);
+};
+
+export default addTask;
