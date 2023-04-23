4a37e2ddd67bf6ab4699da4078e825d303c3763f
Amir-Aminian <amir_aminian@hotmail.com>
2023-04-23 11:24:52 -0230

    fixed a typo in clear cookies file in server side

--- server/routes/clearCookies.js
+++ server/routes/clearCookies.js
@@ -1,6 +1,12 @@
-const clearCookies = (req, res) => {
-  res.status(200).clearCookie("access_token");
-  return(res.status(200).json("Successfully logged out."));
-}
+const express = require("express");
+const router = express.Router();
 
-module.exports = clearCookies;
+router.get(
+  "/", 
+  (req, res) => {
+    res.status(200).clearCookie("access_token");
+    return(res.status(200).json("You have successfully logged out."));
+  }
+);
+
+module.exports = router;
