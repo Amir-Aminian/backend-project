28d229f1b789475bbae4cb988db8e3a6bf648161
Amir-Aminian <amir_aminian@hotmail.com>
2023-04-26 18:13:41 -0230

    added dot env file to git ignore file

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 const static__dir = path.resolve(path.join(__dirname, "../client/build"));
-const port = process.env.PORT;
+const port = process.env.PORT || "8080";
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
